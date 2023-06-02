057e8aeac8307e0cb9f80cbf0a160a8ce06cd51e
Лилиана <lilya_gazina@mail.ru>
2023-06-02 19:22:34 +0300

    1commit

---
+++ src/main/java/com/example/corseworke2/CorseWorke2Application.java
@@ -0,0 +1,13 @@
+package com.example.corseworke2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorseWorke2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorseWorke2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corseworke2/CorseWorke2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corseworke2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorseWorke2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
